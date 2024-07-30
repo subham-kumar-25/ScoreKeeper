67399181e89946286a60091d7f4a257ee80fc433
subham-kumar-25 <145878998+subham-kumar-25@users.noreply.github.com>
2024-07-30 15:19:10 +0000

    Count of correct and incorrect Score

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let score = 0;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
 function increaseScore() {
+    correctAnswers++;
     score++;
     updateScore();
 }
 
 function decreaseScore() {
+    incorrectAnswers++;
     score--;
     updateScore();
 }
 
 function updateScore() {
     document.getElementById('score').textContent = score;
+    document.getElementById('correctScore').textContent = "Correct : " + correctAnswers;
+    document.getElementById('incorrectScore').textContent =  "Incorrect : " + incorrectAnswers;
 }
